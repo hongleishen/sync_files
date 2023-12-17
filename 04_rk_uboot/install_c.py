2cf5b8cbe01139b2ed111590de54d78d5a5f4456
shl <shladi@126.com>
2023-12-17 14:09:25 +0800

    money update to 1217
    Signed-off-by: shl <shladi@126.com>

---
+++ 04_rk_uboot/install_c.py
@@ -0,0 +1,14 @@
+sudo apt-get install uuid uuid-dev zlib1g-dev liblz-dev liblzo2-2 liblzo2-dev lzop \
+git curl u-boot-tools mtd-utils  openjdk-8-jdk device-tree-compiler \
+gdisk m4  git gnupg flex bison gperf libsdl1.2-dev libesd-java  \
+squashfs-tools build-essential zip curl libncurses5-dev zlib1g-dev pngcrush schedtool \
+libxml2 libxml2-utils xsltproc lzop libc6-dev schedtool g++-multilib lib32z1-dev \
+lib32ncurses-dev lib32readline-dev gcc-multilib libswitch-perl libssl-dev unzip \
+zip liblz4-tool  git ssh make gcc libssl-dev liblz4-tool vim expect \
+g++ patchelf chrpath gawk texinfo chrpath diffstat binfmt-support \
+qemu-user-static live-build bison flex fakeroot cmake gcc-multilib g++-multilib \
+unzip device-tree-compiler  libncurses5-dev rsync subversion \
+sed make binutils  build-essential  gcc  g++  wget python-is-python2 libncurses5 bzr cvs git mercurial \
+patch gzip bzip2 perl tar cpio unzip rsync file bc wget qemu-user-static live-build -y
+
+# git-core  libz-dev lib32ncurses5-dev python android-tools-fsutils libwxgtk3.0-dev python-pip zlibz-dev  repo
